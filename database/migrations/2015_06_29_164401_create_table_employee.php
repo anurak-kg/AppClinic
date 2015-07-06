62bcb1b0b85f98e9f8be002350605b0f1645a248
hlkittipan <hlkittipan@gmail.com>
2015-07-06 14:15:59 +0700

    update FK

---
+++ database/migrations/2015_06_29_164401_create_table_employee.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableEmployee extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->string('emp_id',4);
+            $table->string('branch_id',3);
+            $table->string('emp_name',30);
+            $table->string('emp_lastname',30);
+            $table->string('emp_position',10);
+            $table->string('emp_tel',10);
+            $table->string('emp_sex',5);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('employee');
+    }
+}
