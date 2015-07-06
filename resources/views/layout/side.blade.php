3cf904c6c60170a3f2e038ea6406a4e25d83f5d6
hlkittipan <hlkittipan@gmail.com>
2015-07-06 14:34:41 +0700

    update FK edit PK from INT to Varchar edit คอร์า to คอร์ส

--- resources/views/layout/side.blade.php
+++ resources/views/layout/side.blade.php
@@ -19,7 +19,7 @@
             <li class="treeview">
                 <a href="#"><i class='fa fa-database'></i> <span>Course</span> <i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
-                    <li><a href="#">จัดการคอร์า</a></li>
+                    <li><a href="#">จัดการคอร์ส</a></li>
                 </ul>
             </li>
             <li class="treeview">
