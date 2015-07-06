d97738241c4f290cd855bd3f85404a3cec3c5e5d
hlkittipan <hlkittipan@gmail.com>
2015-07-06 23:40:15 +0700

    test course

---
+++ resources/views/course.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.master')
+@section('title','Dashboard')
+@section('headText','Dashboard')
+@section('headDes','รายละเอียด')
+@section('content')
+
+    <div class="row">
+
+    </div>
+@stop
