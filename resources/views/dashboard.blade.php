a654b1942d4e8d403b5bbb39d5586ef332934f3f
Salmon <recaro-phenix@hotmail.com>
2015-07-16 22:11:05 +0700

    แก้ไข หน้า home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -65,31 +65,11 @@
 
      </div>
 
-       <meta charset="UTF-8">
-         <title>ตารางการทำงานหมอ</title>
-         <!-- Tell the browser to be responsive to screen width -->
-         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-         <!-- Bootstrap 3.3.4 -->
-         <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-         <!-- Font Awesome Icons -->
-         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-         <!-- Ionicons -->
-         <link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+
+
          <!-- fullCalendar 2.2.5-->
          <link href="../plugins/fullcalendar/fullcalendar.min.css" rel="stylesheet" type="text/css" />
          <link href="../plugins/fullcalendar/fullcalendar.print.css" rel="stylesheet" type="text/css" media="print" />
-         <!-- Theme style -->
-         <link href="../dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
-         <!-- AdminLTE Skins. Choose a skin from the css/skins
-              folder instead of downloading all of them to reduce the load. -->
-         <link href="../dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
-
-         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-         <!--[if lt IE 9]>
-             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-         <![endif]-->
 
 
              <!-- Main content -->
@@ -99,7 +79,7 @@
                    <div class="box box-solid box-success">
                    <div class="box-header with-border" align="middle">
 
-                                       <h3 class="box-title">ตารางหมอ</h3>
+                                       <h3 class="box-title">ตารางการทำงานหมอ</h3>
                                        <div class="box-tools pull-right">
                                              <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" ><i class="fa fa-minus"></i></button>
 
@@ -116,7 +96,7 @@
 
                                 <div class="box box-solid box-default">
                                         <div class="box-header with-border">
-                                          <h3 class="box-title">ประเภทคอร์ส</h3>
+                                          <h3 class="box-title">สรุปประเภทคอร์ส</h3>
                                           <div class="box-tools pull-right">
                                             <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
                                           </div>
@@ -151,7 +131,7 @@
 
                                               <div class="box box-solid box-default">
                                                                 <div class="box-header with-border">
-                                                                  <h3 class="box-title">รายงานยอดขายแต่ละสาขา</h3>
+                                                                  <h3 class="box-title">รายงานยอดขาย</h3>
                                                                   <div class="box-tools pull-right">
                                                                     <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
 
