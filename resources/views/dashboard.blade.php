418ca28fde437d7c2499c122bf2ae0a000dcc7a2
Salmon <recaro-phenix@hotmail.com>
2015-07-16 18:20:56 +0700

    แก้ไข error home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -291,20 +291,19 @@
              });
            });
          </script>
- <!-- ChartJS 1.0.1 -->
-    <script src="plugins/chartjs/Chart.min.js" type="text/javascript"></script>
+         <!-- ChartJS 1.0.1 -->
+            <script src="plugins/chartjs/Chart.min.js" type="text/javascript"></script>
      <!-- jvectormap -->
         <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js" type="text/javascript"></script>
         <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js" type="text/javascript"></script>
         <!-- SlimScroll 1.3.0 -->
         <script src="plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>
 
- <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-    <script src="../../plugins/morris/morris.min.js" type="text/javascript"></script>
-    <!-- FastClick -->
-    <script src="../../plugins/fastclick/fastclick.min.js" type="text/javascript"></script>
-    <!-- AdminLTE App -->
-    <script src="../../dist/js/app.min.js" type="text/javascript"></script>
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+
+     <script src="../../plugins/morris/morris.min.js" type="text/javascript"></script>
+
+
     <!-- AdminLTE for demo purposes -->
     <script src="../../dist/js/demo.js" type="text/javascript"></script>
          <script type="text/javascript">
