ff8f04b38cf466d5f0cc1a0abfa75ff00ac50567
hlkittipan <hlkittipan@gmail.com>
2015-07-19 12:21:28 +0700

    edit FK customer

--- app/Customer.php
+++ app/Customer.php
@@ -13,22 +13,22 @@ class Customer extends Model
 
     public function Quotations()
     {
-        return $this->hasmany('\App\Quotations','quo_id');
+        return $this->hasmany('\App\Quotations','cus_id');
     }
 
     public function Treatment()
     {
-        return $this->hasmany('\App\Treatment','tre_id');
+        return $this->hasmany('\App\Treatment','cus_id');
     }
 
     public function Disease_detail ()
     {
-        return $this->hasmany('\App\Disease_detail','dis_de_id');
+        return $this->hasmany('\App\Disease_detail','cus_id');
     }
 
     public function Allergic_detail ()
     {
-        return $this->hasmany('\App\Allergic_detail','gic_de_id');
+        return $this->hasmany('\App\Allergic_detail','cus_id');
     }
 
 }
