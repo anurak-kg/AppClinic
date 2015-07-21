cb18b0051630e2df37f58b6ab05fa74e5e2b1867
kurodo99 <imannn.99@gmail.com>
2015-07-21 16:49:17 +0700

    restore course

--- app/Course.php
+++ app/Course.php
@@ -8,6 +8,9 @@ class Course extends Model
 {
     protected $table = 'course';
     protected $primaryKey = 'course_id';
-
+    public function detail()
+    {
+        return $this->hasMany('App\Course_detail');
+    }
 
 }
