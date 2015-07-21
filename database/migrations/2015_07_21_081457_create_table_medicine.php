5b82de8f4861bf67d2818e5a8ded5a39654cfff7
hlkittipan <hlkittipan@gmail.com>
2015-07-21 15:20:55 +0700

    แก้ไข DB รายละเอียดการใช้ยา/คอร์ส

---
+++ database/migrations/2015_07_21_081457_create_table_medicine.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableMedicine extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicine', function (Blueprint $table) {
+            $table->increments('med_id');
+            $table->integer('course_id');
+            $table->string('product_id');
+            $table->string('product_name');
+            $table->integer('qty');//จำนวนยาที่ใช้
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('medicine');
+    }
+}
