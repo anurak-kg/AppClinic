e03f99a6331c02cb24f637c1a48ff51048a2bb22
hlkittipan <hlkittipan@gmail.com>
2015-07-21 13:21:05 +0700

    เพิ่ม DB ผ่อนชำระ

---
+++ database/migrations/2015_07_21_061618_create_table_installment.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableInstallment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('installment', function (Blueprint $table) {
+            $table->increments('ins_id');
+            $table->integer('ins_price');//จำนวนจ่าย
+            $table->integer('ins_balance');//คงเหลือ
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
