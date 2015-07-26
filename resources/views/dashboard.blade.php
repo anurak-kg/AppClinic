b80991b26321a41dfa224e81efc158285ca7dc35
Salmon <recaro-phenix@hotmail.com>
2015-07-26 12:23:47 +0700

    home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -107,7 +107,7 @@
 
                                 <div class="box  box-default">
                                         <div class="box-header with-border">
-                                          <h3 class="box-title">สรุปคอร์ส</h3>
+                                          <h3 class="box-title">คอร์สชายดี</h3>
                                           <div class="box-tools pull-right">
                                             <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
                                           </div>
