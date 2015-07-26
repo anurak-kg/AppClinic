5a22589e31c534946df2d2ada4e61cfba09b962f
Salmon <recaro-phenix@hotmail.com>
2015-07-27 00:29:17 +0700

    แก้ไข Report

--- resources/views/report/coursemonth.blade.php
+++ resources/views/report/coursemonth.blade.php
@@ -5,32 +5,12 @@
 
     <div class="row">
 
-        <div class="row">
 
-            <div class="col-md-3">
-                <div class="box box-solid box-default">
-                    <div class="box-body">
-                        {!! Form::open(array('url' => 'report/coursemonth', 'class' => 'form')) !!}
-                        <div class="form-group">
-                            <div class="input-group">
-                                <label>กำหนดระยะเวลา</label><br>
-                                <input class="btn btn-default pull-right" id="daterange" name="rang">
-                                </input><br><br>
-                                <div align="left"> <input type="submit"  class="btn btn-primary" value="Report"></div>
-                            </div>
-                        </div>
-                        {!! Form::close() !!}
-                        <!-- /.form group -->
-
-                    </div>
-                </div>
-            </div>
 
         <div class="col-md-12">
 
-
             <!-- BAR CHART -->
-            <div class="box box-solid box-default">
+            <div class="box box-default">
                 <div class="box-header with-border">
                     <h4 align="middle">ยอดขายคอร์ส</h4>
 
@@ -39,10 +19,23 @@
                     </div>
                 </div>
                 <div class="box-body">
+
+                    {!! Form::open(array('url' => 'report/coursemonth', 'class' => 'form')) !!}
+                    <div class="form-group">
+                        <div class="input-group">
+                            <label>กำหนดระยะเวลา</label><br>
+                            <input class="btn btn-default pull-right" id="daterange" name="rang" placeholder="เลือกระยะเวลา..">
+                            </input><br><br>
+                            <div align="left"> <input type="submit"  class="btn btn-block btn-primary" value="แสดง"></div>
+                        </div>
+                    </div>
+                    {!! Form::close() !!}
+
                     <div class="chart">
-                        <canvas id="barChart" height="700"></canvas>
+                        <canvas id="barChart" height="500"></canvas>
                     </div>
                 </div>
+
                 <!-- /.box-body -->
             </div>
             <!-- /.box -->
@@ -114,8 +107,8 @@
             var barChartCanvas = $("#barChart").get(0).getContext("2d");
             var barChart = new Chart(barChartCanvas);
             var barChartData = areaChartData;
-            barChartData.datasets[0].fillColor = "#00a65a";
-            barChartData.datasets[0].strokeColor = "#00a65a";
+            barChartData.datasets[0].fillColor = "#c2ffd5";
+            barChartData.datasets[0].strokeColor = "#11ff5b";
             barChartData.datasets[0].pointColor = "#00a65a";
             var barChartOptions = {
                 //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
