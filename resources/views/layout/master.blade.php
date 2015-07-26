857f6041797bb108b319fdbbb35addac5d367e60
kurodo99 <imannn.99@gmail.com>
2015-07-26 13:35:50 +0700

    add css.less

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -27,7 +27,6 @@
     <link rel="stylesheet" href="/dist/js/ng-table.css">
     <link href="/dist/css/skins/skin-blue.min.css" rel="stylesheet" type="text/css"/>
     <link href="/dist/css/typeahead.css" rel="stylesheet" type="text/css" />
-    <link href="/app.css" rel="stylesheet" type="text/css"/>
     <link href="https://almsaeedstudio.com/themes/AdminLTE/plugins/datatables/dataTables.bootstrap.css
 " rel="stylesheet" type="text/css"/>
 
@@ -43,6 +42,8 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <link href="/css/app.css" rel="stylesheet" type="text/css"/>
 
 
 
