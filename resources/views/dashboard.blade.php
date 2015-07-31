c68c343905f5b8b28d4ffce2be53f27b7fa7285b
Admin <recaro1236@gmail.com>
2015-07-31 15:38:21 +0700

    fix home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -115,7 +115,7 @@
                                           <div class="row">
                                             <div class="col-md-8">
                                               <div class="chart-responsive">
-                                                <canvas id="pieChart" height="150"></canvas>
+                                                <canvas id="pieChart" height="100"></canvas>
                                               </div><!-- ./chart-responsive -->
                                             </div><!-- /.col -->
                                             <div class="col-md-4">
@@ -140,42 +140,19 @@
                                       </div><!-- /.box -->
                  </div>
                    <div class="col-md-6">
-                       <div class="box  box-default">
-                           <div class="box-header with-border">
-                               <h3 class="box-title">สรุปคอร์สขายดี</h3>
-                               <div class="box-tools pull-right">
-                                   <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-                               </div>
-                           </div><!-- /.box-header -->
-                           <div class="box-body">
-                               <div class="row">
-                                   <div class="col-md-8">
-                                       <div class="chart-responsive">
-                                           <canvas id="pieChart1" height="150"></canvas>
-                                       </div><!-- ./chart-responsive -->
-                                   </div><!-- /.col -->
-                                   <div class="col-md-4">
-                                       <ul class="chart-legend clearfix">
-                                           <hr>
-                                           <li><i class="fa fa-circle-o text-red"></i> MELASMA WHITENING</li>
-                                           <li><i class="fa fa-circle-o text-green"></i> ACNE</li>
-                                           <li><i class="fa fa-circle-o text-yellow"></i> AGE REVERSE</li>
-                                           <li><i class="fa fa-circle-o text-aqua"></i> VELA BODY</li>
-                                       </ul>
-                                   </div><!-- /.col -->
-                               </div><!-- /.row -->
-                           </div><!-- /.box-body -->
-                           <div class="box-footer no-padding">
-                               <ul class="nav nav-pills nav-stacked">
-                                   <li><a href="#">MELASMA WHITENING<span class="pull-right text-red"><i class="fa fa-angle-down"></i> 30%</span></a></li>
-                                   <li><a href="#">ACNE <span class="pull-right text-green"><i class="fa fa-angle-up"></i> 20%</span></a></li>
-                                   <li><a href="#">AGE REVERSE <span class="pull-right text-yellow"><i class="fa fa-angle-left"></i> 20%</span></a></li>
-                                   <li><a href="#">VELA BODY <span class="pull-right text-yellow"><i class="fa fa-angle-left"></i> 30%</span></a></li>
-                               </ul>
-                           </div><!-- /.footer -->
-                       </div><!-- /.box -->
-                   </div>
+                                              <div class="box  box-default">
+                                                                <div class="box-header with-border">
+                                                                  <h3 class="box-title">รายงานยอดขาย</h3>
+                                                                  <div class="box-tools pull-right">
+                                                                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
 
+                                                                  </div>
+                                                                </div>
+                                                                <div class="box-body chart-responsive">
+                                                                  <div class="chart" id="bar-chart" style="height: 300px;"></div>
+                                                                </div><!-- /.box-body -->
+                                                              </div><!-- /.box -->
+                   </div>
                    <div class="col-md-6">
                    <!-- PRODUCT LIST -->
                    <div class="box box-primary">
@@ -239,11 +216,7 @@
                        </div><!-- /.box-footer -->
                    </div><!-- /.box -->
                    </div>
-
                </div><!-- /.row -->
-
-
-
 
     <!-- fullCalendar 2.2.5-->
     <link href="../plugins/fullcalendar/fullcalendar.min.css" rel="stylesheet" type="text/css"/>
@@ -598,39 +571,6 @@
                          //Create pie or douhnut chart
                          // You can switch between pie and douhnut using the method below.
                          pieChart.Doughnut(PieData, pieOptions);
-
-
-
-               var pieChartCanvas = $("#pieChart1").get(0).getContext("2d");
-               var pieChart = new Chart(pieChartCanvas);
-               var PieData = {!! $dataCourse !!};
-               var pieOptions = {
-                   //Boolean - Whether we should show a stroke on each segment
-                   segmentShowStroke: true,
-                   //String - The colour of each segment stroke
-                   segmentStrokeColor: "#fff",
-                   //Number - The width of each segment stroke
-                   segmentStrokeWidth: 2,
-                   //Number - The percentage of the chart that we cut out of the middle
-                   percentageInnerCutout: 50, // This is 0 for Pie charts
-                   //Number - Amount of animation steps
-                   animationSteps: 100,
-                   //String - Animation easing effect
-                   animationEasing: "easeOutBounce",
-                   //Boolean - Whether we animate the rotation of the Doughnut
-                   animateRotate: true,
-                   //Boolean - Whether we animate scaling the Doughnut from the centre
-                   animateScale: false,
-                   //Boolean - whether to make the chart responsive to window resizing
-                   responsive: true,
-                   // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-                   maintainAspectRatio: false,
-                   //String - A legend template
-                   legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
-               };
-               //Create pie or douhnut chart
-               // You can switch between pie and douhnut using the method below.
-               pieChart.Doughnut(PieData, pieOptions);
 
                  //BAR CHART
                  var bar = new Morris.Bar({
