224ec8e03eee0fc0141ce808e1cb9c8719febd13
kurodo99 <imannn.99@gmail.com>
2015-08-23 01:55:52 +0700

    quo database vat update

--- database/migrations/2015_06_30_104558_create_table_quotations.php
+++ database/migrations/2015_06_30_104558_create_table_quotations.php
@@ -26,7 +26,7 @@ class CreateTableQuotations extends Migration
             // 2 = อยุ่ในระหว่างการรักษา ,3 = เรียบร้อยแล้ว
             //VAT
             $table->decimal('total_net_price',14,2);
-            $table->enum('vat_type', array('none','in_vat','out_vat'));
+            $table->boolean('vat');
             $table->integer('vat_rate');//
             $table->integer('commission_rate');//% ค่าคอมมิชชั้น
             $table->integer('branch_id')->nullable();
