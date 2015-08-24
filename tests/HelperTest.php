71c3c98ce097f36e73b84f74297616c4848d9dbf
kurodo99 <imannn.99@gmail.com>
2015-08-25 03:31:28 +0700

    test case update

--- tests/HelperTest.php
+++ tests/HelperTest.php
@@ -9,6 +9,15 @@ class HelperTest extends TestCase
             ->press('เข้าสู่ระบบ');
         $this->assertEquals(10,strlen(getNewQuoPK()));
         $this->assertEquals(10,strlen(getNewQuoDetailPK()));
+        $this->assertEquals(10,strlen(getNewSalePK()));
+        $this->assertEquals(10,strlen(getNewOrderPK()));
+        $this->assertEquals(10,strlen(getNewReceivePK()));
+        $this->assertEquals(10,strlen(getNewReturnPK()));
+        $this->assertEquals(10,strlen(getNewInvTranPK()));
+        $this->assertEquals(10,strlen(getNewPaymentPK()));
+        $this->assertEquals(10,strlen(getNewPaymentDetailPK()));
+        //$this->assertEquals(10,strlen(getNewCustomerPK()));
+
     }
 
 }
