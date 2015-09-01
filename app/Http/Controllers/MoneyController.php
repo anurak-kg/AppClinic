8acbcfcbae1f71b08bbfd6f6530a2c53a82790eb
Salmon <recaro1236@gmail.com>
2015-09-01 13:10:40 +0700

    หน้าการจัดการเงิน

--- app/Http/Controllers/MoneyController.php
+++ app/Http/Controllers/MoneyController.php
@@ -17,22 +17,31 @@ class MoneyController extends Controller
     }
 
     public function moneyDr(){
-        $data =DB::table('treat_history')
-            ->select('treat_history.dr_id','users.name',DB::raw('SUM(treat_history.dr_price) as Total'))
-            ->join('users','users.id','=','treat_history.dr_id')
-            ->where('users.position_id','=',4)
-            ->groupby('treat_history.dr_id')
-        ->get();
+        $rang = \Input::get('rang');
+        $date = explode('to', $rang);
+
+        $datadr =DB::table('users')
+            ->select('users.id','users.name'
+                ,DB::raw('(SELECT SUM(bt1_price) FROM treat_history WHERE treat_history.bt_user_id1 = id) as bt1')
+                ,DB::raw('(SELECT SUM(bt2_price) FROM treat_history WHERE treat_history.bt_user_id2 = id) as bt2'))
+            ->where('users.position_id','=',4);
+                 if ($rang != null) {
+                     $datadr->whereRaw("DATE(quotations_detail.created_at) between ? and ?", [trim($date[0]), trim($date[1])]);
+                     $dateTxt['start'] = Date::createFromFormat("Y-m-d", trim($date[0]))->format('l j F Y');
+                     $dateTxt['end'] = Date::createFromFormat("Y-m-d", trim($date[1]))->format('l j F Y');
+                 }
+
+        $data=  $datadr->get();
 
         $data1 =DB::table('users')
             ->select('users.id','users.name'
                 ,DB::raw('(SELECT SUM(bt1_price) FROM treat_history WHERE treat_history.bt_user_id1 = id) as bt1')
             ,DB::raw('(SELECT SUM(bt2_price) FROM treat_history WHERE treat_history.bt_user_id2 = id) as bt2'))
-            ->where('users.position_id','<>',4)
+            ->where('users.position_id','=',8)
             ->get();
 
 
-        //return response()->json($data1);
+        //return response()->json($data);
 
         return view('money/manage',['data'=>$data,'data1'=>$data1]);
     }
