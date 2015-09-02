26f9d9db44c55eeb031881c2ee7cbf14c96035e5
kurodo99 <imannn.99@gmail.com>
2015-09-02 14:32:24 +0700

    แก้ model bt ขึ้นต้นด้วยตัวพิมใหญ่

---
+++ app/Bt.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bt extends Model
+{
+    protected $table = 'bt';
+    protected $primaryKey = 'bt_id';
+
+
+    public function TreatHistory(){
+        return $this->belongsTo('\App\TreatHistory', 'treat_id');
+    }
+    public function User(){
+        return $this->belongsTo('\App\User', 'emp_id');
+    }
+
+
+}
