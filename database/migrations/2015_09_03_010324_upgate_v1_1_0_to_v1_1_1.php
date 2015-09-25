65ae4f2e9f7f92138dc54d93dda184d6b568658b
hlkittipan <hlkittipan@gmail.com>
2015-09-25 16:38:53 +0700

    add migrate

--- database/migrations/2015_09_03_010324_upgate_v1_1_0_to_v1_1_1.php
+++ database/migrations/2015_09_03_010324_upgate_v1_1_0_to_v1_1_1.php
@@ -17,16 +17,8 @@ class UpgateV110ToV111 extends Migration
 
         DB::unprepared("ALTER TABLE `payment_detail`
                         MODIFY COLUMN `payment_type`  enum('CASH','DEBIT','Transfer','CREDIT')
-                        CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL AFTER `branch_id`;
+                        CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL AFTER `branch_id`;");
 
-");
-
-        DB::unprepared("ALTER TABLE `payment_detail`
-                        ADD COLUMN `id_account`  varchar(255) NOT NULL AFTER `updated_at`,
-                        ADD COLUMN `transfer_day`  date NOT NULL AFTER `id_account`,
-                        ADD COLUMN `transfer_hour`  varchar(255) NOT NULL AFTER `transfer_day`,
-                        ADD COLUMN `transfer_min`  varchar(255) NOT NULL AFTER `transfer_hour`;
-                        ");
 
     }
 
@@ -38,11 +30,7 @@ class UpgateV110ToV111 extends Migration
     public function down()
     {
         DB::unprepared("ALTER TABLE `order`   DROP COLUMN `bill_number` ");
-        DB::unprepared("ALTER TABLE `payment_detail`
-                        DROP COLUMN `id_account`,
-                        DROP COLUMN `transfer_day`,
-                        DROP COLUMN `transfer_day`,
-                        DROP COLUMN `transfer_day`");
+
 
     }
 }
