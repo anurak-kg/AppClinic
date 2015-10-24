ee5aebefd6806da9ccbafeddb7fedb66469ac13d
hlkittipan <hlkittipan@gmail.com>
2015-10-24 15:17:49 +0700

    th-en สินค้า

---
+++ resources/lang/en/product.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Master
+ * Date: 24/10/2015
+ * Time: 03:16 PM
+ */
+return [
+    'product'=>'Product',
+    'product code'=>'Product Code',
+    'Product groups'=>'Product Groups',
+    'Product name'=>'Product Name',
+    'Order point'=>'Order point',
+    'price / Unit'=>'Price / Unit',
+    'Unit'=>'Unit',
+    'Action'=>'Action',
+    'Add'=>'Add',
+    'Medication and Product Management'=>'Medication and Product Management',
+
+];
---
+++ resources/lang/th/product.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Master
+ * Date: 24/10/2015
+ * Time: 03:08 PM
+ */
+return [
+    'product'=>'สินค้า',
+    'product code'=>'รหัสสินค้า',
+    'Product groups'=>'กลุ่มสินค้า',
+    'Product name'=>'ชื่อสินค้า',
+    'Order point'=>'Order point',
+    'price / Unit'=>'ราคา/หน่วย',
+    'Unit'=>'หน่วยนับ',
+    'Action'=>'กระทำ', 'Add'=>'เพิ่มข้อมูลใหม่',
+    'Medication and Product Management'=>'จัดการยาและสินค้า',
+
+];
