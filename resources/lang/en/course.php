c2be0212555af7c1203118f9fde87f280eb3ecda
hlkittipan <hlkittipan@gmail.com>
2015-10-25 15:00:07 +0700

    en คอร์ส

--- resources/lang/en/course.php
+++ resources/lang/en/course.php
@@ -5,3 +5,22 @@
  * Date: 25/10/2015
  * Time: 02:26 PM
  */
+return [
+    'Course Management'=>'Course Management',
+    'add'=>'Add',
+    'course code'=>'Course Code',
+    'course name'=>'Course Name',
+    'qty'=>'Qty',
+    'qty course'=>'Course Qty',
+    'price'=>'Price',
+    'Item Information'=>'Item Information',
+    'Action'=>'Action',
+    'medicine'=>'Medicine',
+    'medicine code'=>'Medicine Code',
+    'back'=>'Back',
+    'detail'=>'*If the course does not require medication. Save it.',
+    'Commission'=>'Commission',
+    'course category'=>'Course Category',
+    'course category code'=>'Course Category Code',
+    'course category name'=>'Course Category Name',
+];
