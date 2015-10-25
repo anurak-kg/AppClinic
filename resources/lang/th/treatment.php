de3e18f390f2203204227cd5a218f68c1231249d
kurodo99 <imannn.99@gmail.com>
2015-10-25 16:39:31 +0700

    added : แปลภาษา Treatment  th/en

---
+++ resources/lang/th/treatment.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'treatment' => 'การรักษา',
+    'course_detail' => 'ข้อมูลคอร์ส',
+    'course' => 'คอร์ส',
+    'quantity' => 'จำนวน',
+    'outstanding_balance' => 'ยอดค้างชำระ',
+    'treatment_already' => 'รักษาไปแล้ว',
+    'status' => 'สถานะ',
+
+
+];
