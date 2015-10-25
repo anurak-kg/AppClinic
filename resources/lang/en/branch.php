59f02074513f896a7609430f00881254ad96d4f6
hlkittipan <hlkittipan@gmail.com>
2015-10-25 15:06:37 +0700

    th-en สาขา

---
+++ resources/lang/en/branch.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Master
+ * Date: 25/10/2015
+ * Time: 03:01 PM
+ */
+return [
+    'branch Code'=>'Branch Code',
+    'branch name'=>'Branch Name',
+    'Add'=>'Add',
+    'address'=>'Address',
+    'tel'=>'Phone Number',
+    'Tax Identification Number'=>'Tax Identification Number',
+    'Action'=>'Action',
+];
---
+++ resources/lang/th/branch.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Master
+ * Date: 25/10/2015
+ * Time: 03:01 PM
+ */
+return [
+    'branch Code'=>'รหัสสาขา',
+    'branch name'=>'ชื่อสาขา',
+    'Add'=>'เพิ่มข้อมูล',
+    'address'=>'ที่อยู่',
+    'tel'=>'เบอร์โทรศัพท์',
+    'Tax Identification Number'=>'หมายเลขประจำตัวผู้เสียภาษี',
+    'Action'=>'กระทำ',
+];
